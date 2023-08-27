9a58af2d6c4bb395281cc0959dfb568d22772359
Vladimir <plategi@yandex.ru>
2023-08-27 14:40:54 +0200

    o

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Index(){
     return(
         <div>
         <h1>Hi Vovan! How are you?</h1>
         <p>Проверка русского языка внутри React</p>
+        <p><Link href={"/works"} title={'Works'}>Works</Link></p>
         </div>
     )
 }
--- pages/works/index.js
+++ pages/works/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Worsks(){
     return(
         <div>
             <h1>It's page about works</h1>
+            <p><Link href={"/"} title={'Index'}>Index</Link></p>
         </div>
     );
 }
