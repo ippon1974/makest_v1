7b529220818ad4908891b7718cfebbe20276889f
Vladimir <plategi@yandex.ru>
2023-08-27 14:28:42 +0200

    home add pages

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index(){
+    return(
+        <div>
+        <h1>Hi Vovan! How are you?</h1>
+        <p>Проверка русского языка внутри React</p>
+        </div>
+    )
+}
---
+++ pages/works/index.js
@@ -0,0 +1,7 @@
+export default function Worsks(){
+    return(
+        <div>
+            <h1>It's page about works</h1>
+        </div>
+    );
+}
