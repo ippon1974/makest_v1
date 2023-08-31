53eb3e79d3e2671b9ecf68d58b79f24158ad9079
Vladimir <plategi@yandex.ru>
2023-08-31 21:46:57 +0300

    o

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import Head from 'next/head';
 
 export default function Index(){
     return(
+        
         <div>
+            <Head>
+                <title>Лестницы из металла. Лестницы на второй этаж. Производство.</title>
+                <meta name="viewport" content="width=device-width"/>
+                <meta name="description" content={"Производство металлических лестниц в Москвовской области"} />
+            </Head>
         <h1>Лестницы из металла. Лестницы на второй этаж. Производство.</h1>
         <p>Телефон: + 7 (925) 585-33-71</p>
         <p>Пн-Пт с 9:00 до 19:00 </p>
