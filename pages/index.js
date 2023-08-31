9282ae42283cbd9c32b909edc6cd15c102f8a085
Vladimir <plategi@yandex.ru>
2023-08-31 20:11:06 +0300

    o

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default function Index(){
     return(
         <div>
-        <h1>Hi Vovan! How are you?</h1>
-        <p>Проверка русского языка внутри React</p>
-        <p><Link href={"/works"} title={'Works'}>Works</Link></p>
+        <h1>Makestairs.ru</h1>
         </div>
     )
 }
